9bc49f579660a701a1b1fe1e39b18fcf0d3bd48a
Evgeniy <vip.vlas.2000@mail.ru>
2022-11-11 00:21:22 +0300

    Task bs

--- src/BasicSyntax.js
+++ src/BasicSyntax.js
@@ -4,5 +4,63 @@ export function romanToInteger(str) {
     Напишите код, преобразующий число из римской записи в арабскую.
     На вход вы получите строку, ответом должно быть число.
      */
+
+    let strArr = str.split('');
+    for (let i = 0; i < strArr.length; ++i) {
+        switch (strArr[i]) {
+            case 'M':
+                result += 1000;
+                break;
+            case 'D':
+                result += 500;
+                break;
+            case 'C':
+                if (strArr[i + 1] == 'D') {
+                    result += 400;
+                    i++;
+                    break;
+                }
+                if (strArr[i + 1] == 'M') {
+                    result += 900;
+                    i++;
+                    break;
+                }
+                result += 100;
+                break;
+            case 'L':
+                result += 50;
+                break;
+            case 'X':
+                if (strArr[i + 1] == 'L') {
+                    result += 40;
+                    i++;
+                    break;
+                }
+                if (strArr[i + 1] == 'C') {
+                    result += 90;
+                    i++;
+                    break;
+                }
+                result += 10;
+                break;
+            case 'V':
+                result += 5;
+                break;
+            case 'I':
+                if (strArr[i + 1] == 'V') {
+                    result += 4;
+                    i++;
+                    break;
+                }
+                if (strArr[i + 1] == 'X') {
+                    result += 9;
+                    i++;
+                    break;
+                }
+                result += 1;
+                break;
+        }
+    }
+
     return result;
 }
